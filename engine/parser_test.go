b4461fd2e0564ea973e3f0aa219929fb54a3fce6
Chris Kolenko <chris@webcanvas.com.au>
2015-12-10 19:31:32 +1100

    Fix parser tests

--- engine/parser_test.go
+++ engine/parser_test.go
@@ -1,61 +1,36 @@
 package engine
 
-// ToMap parses a string to a map
-func ToMap(str string) map[string]string {
-	m := make(map[string]string)
-	MergeMap(m, str)
-	return m
-}
+import (
+	"reflect"
+	"testing"
+)
 
-// MergeMap parses a string and adds them to a map
-func MergeMap(dest map[string]string, str string) {
-	var groupingch *rune
-	iskey := true
-	key := ""
-	value := ""
+func TestParser(t *testing.T) {
+	s1 := " hello=world "
+	s2 := "'something'=\"yep else\" something2=\"hello  =  = something\""
+	s3 := " "
+	s4 := "hello \"what's\" up"
 
-	for _, ch := range str {
-		switch {
-		case ch == '"' || ch == '\'':
-			if groupingch == nil {
-				cpy := rune(ch)
-				groupingch = &cpy
-				continue
-			}
+	p1 := ToMap(s1)
+	p2 := ToMap(s2)
+	p3 := ToMap(s3)
+	p4 := ToMap(s4)
 
-			if *groupingch == ch {
-				groupingch = nil
-				continue
-			}
+	c1 := map[string]RuntimeVar{"hello": RuntimeVar{"world"}}
+	c2 := map[string]RuntimeVar{"something": RuntimeVar{"yep else"}, "something2": RuntimeVar{"hello  =  = something"}}
+	c3 := map[string]RuntimeVar{}
+	c4 := map[string]RuntimeVar{"hello": RuntimeVar{""}, "what's": RuntimeVar{""}, "up": RuntimeVar{""}}
 
-			fallthrough
-		case ch == '=':
-			if groupingch == nil {
-				iskey = false
-				continue
-			}
-			fallthrough
-		case ch == ' ':
-			if groupingch == nil {
-				if key != "" {
-					dest[key] = value
-					key = ""
-					value = ""
-					iskey = true
-				}
-				continue
-			}
-			fallthrough
-		default:
-			if iskey {
-				key += string(ch)
-			} else {
-				value += string(ch)
-			}
-		}
+	if !reflect.DeepEqual(p1, c1) {
+		t.Error(s1, p1, c1)
 	}
-
-	if key != "" {
-		dest[key] = value
+	if !reflect.DeepEqual(p2, c2) {
+		t.Error(s2, p2, c2)
+	}
+	if !reflect.DeepEqual(p3, c3) {
+		t.Error(s3, p3, c3)
+	}
+	if !reflect.DeepEqual(p4, c4) {
+		t.Error(s4, p4, c4)
 	}
 }
